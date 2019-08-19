c0f425d56ee044d5cded1cc3ae2bb2b3c5737d92
Chris Olstrom <chris@olstrom.com>
2019-08-19 13:30:43 -0700

    Add time resource

---
+++ lib/concourse-objects/resources/time.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative "../../concourse-objects"
+
+module ConcourseObjects
+  module Resources
+    class Time < Resource
+      RESOURCE_NAME     = "time"
+      GITHUB_OWNER      = "concourse"
+      GITHUB_REPOSITORY = "concourse/time-resource"
+      GITHUB_VERSION    = "v1.2.1"
+
+      class Source < Object
+        DEFAULT_LOCATION = "UTC"
+
+        optional :interval, write: AsString
+        optional :start,    write: AsString
+        optional :stop,     write: AsString
+        optional :location, write: AsString,          default: proc { DEFAULT_LOCATION }
+        optional :days,     write: AsArrayOf.(:to_s), default: EmptyArray
+      end
+      optional :source, write: Source
+
+      def initialize(options = {})
+        super(options.merge(type: "time"))
+      end
+    end
+  end
+end
