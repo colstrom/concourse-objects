1febec52b10428bc0a8e192f8c1355e51047afbd
Chris Olstrom <chris@olstrom.com>
2019-08-22 16:59:56 -0700

    Add newrelic-deploy resource

---
+++ lib/concourse-objects/resources/newrelic-deploy.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative "../../concourse-objects"
+
+module ConcourseObjects
+  module Resources
+    class NewRelicDeploy < Resource
+      RESOURCE_NAME     = "newrelic-deploy"
+      GITHUB_OWNER      = "homedepot"
+      GITHUB_REPOSITORY = "homedepot/newrelic-deploy-resource"
+      GITHUB_VERSION    = "v1.0"
+
+      class Source < Object
+        required :api_key, write: AsString
+        required :app_id,  write: AsString
+      end
+
+      class OutParams < Object
+        required :json_file, write: AsString
+        optional :user,      write: AsString
+      end
+      
+      optional :source, write: Source
+
+      def initialize(options = {})
+        super(options.merge(type: "newrelic-deploy"))
+      end
+    end
+  end
+end
