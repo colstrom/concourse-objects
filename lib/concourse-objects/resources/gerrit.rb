0d42a38a4f5a09a33b4e3489f904ff3ec94e6eaf
Chris Olstrom <chris@olstrom.com>
2019-08-22 16:59:14 -0700

    Add gerrit resource

---
+++ lib/concourse-objects/resources/gerrit.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require_relative "../../concourse-objects"
+
+module ConcourseObjects
+  module Resources
+    class Gerrit < Resource
+      RESOURCE_NAME     = "gerrit"
+      GITHUB_OWNER      = "google"
+      GITHUB_REPOSITORY = "google/concourse-resources"
+      GITHUB_VERSION    = "0.2.2"
+
+      class Source < Object
+        DEFAULT_DIGEST_AUTH = false
+        DEFAULT_QUERY       = "status:open"
+
+        required :uri,         write: AsString
+
+        optional :digest_auth, write: AsBoolean, default: proc { DEFAULT_DIGEST_AUTH }
+        optional :query,       write: AsString,  default: proc { DEFAULT_QUERY }
+        optional :cookies,     write: AsString
+        optional :password,    write: AsString
+        optional :username,    write: AsString
+      end
+
+      class InParams < Object
+        DEFAULT_FETCH_PROTOCOL = "http"
+
+        optional :fetch_protocol, write: AsString, default: proc { DEFAULT_FETCH_PROTOCOL }
+        optional :fetch_url,      write: AsString
+      end
+
+      class OutParams < Object
+        required :repository,   write: AsString
+
+        optional :message,      write: AsString
+        optional :message_file, write: AsString
+        optional :labels,       write: AsHash,  default: EmptyHash
+      end
+
+      optional :source, write: Source
+
+      def initialize(options = {})
+        super(options.merge(type: "gerrit"))
+      end
+    end
+  end
+end
