1ec6dfa43a155402aefa52c8b4f54306fe53d8a1
Chris Olstrom <chris@olstrom.com>
2019-08-26 07:36:15 -0700

    Add calendar resource

---
+++ lib/concourse-objects/resources/calendar.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require_relative "../../concourse-objects"
+
+module ConcourseObjects
+  module Resources
+    class Calendar < Resource
+      RESOURCE_NAME     = "calendar"
+      GITHUB_OWNER      = "henrytk"
+      GITHUB_REPOSITORY = "henrytk/calendar-resource"
+      GITHUB_VERSION    = "v1.1"
+
+      class Source < Object
+        required :provider,    write: AsString
+        required :calendar_id, write: AsString
+        required :event_name,  write: AsString
+        required :credentials, write: AsString
+      end
+
+      class OutParams < Object
+        optional :summary,     write: AsString
+        optional :description, write: AsString
+        optional :time_zone,   write: AsString
+        optional :start_time,  write: AsString
+        optional :end_time,    write: AsString
+      end
+
+      optional :source, write: Source
+
+      def initialize(options = {})
+        super(options.merge(type: "calendar"))
+      end
+    end
+  end
+end
