c3fba71ed9221ac2dd34eb682ad8f9fa9d7bee37
Chris Olstrom <chris@olstrom.com>
2019-08-26 11:09:42 -0700

    Add rocketchat-notification resource

---
+++ lib/concourse-objects/resources/rocketchat-notification.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative "../../concourse-objects"
+
+module ConcourseObjects
+  module Resources
+    class RocketChatNotification < Resource
+      RESOURCE_NAME     = "rocketchat-notification"
+      GITHUB_OWNER      = "michaellihs"
+      GITHUB_REPOSITORY = "michaellihs/rocketchat-notification-resource"
+      GITHUB_VERSION    = "1.0.3"
+
+      class Source < Object
+        DEFAULT_ALIAS = "Concourse"
+        DEFAULT_DEBUG = false
+
+        required :url,      write: AsString
+        required :user,     write: AsString
+        required :password, write: AsString
+
+        optional :channel,  write: AsString
+        optional :alias,    write: AsString,  default: proc { DEFAULT_ALIAS }
+        optional :debug,    write: AsBoolean, default: proc { DEFAULT_DEBUG }
+      end
+
+      class OutParams < Object
+        DEFAULT_ALIAS = "Concourse"
+
+        required :channel, write: AsString
+        required :message, write: AsString
+
+        optional :alias,   write: AsString, default: proc { DEFAULT_ALIAS }
+      end
+
+        optional :source, write: Source
+
+      def initialize(options = {})
+        super(options.merge(type: "rocketchat-notification"))
+      end
+    end
+  end
+end
